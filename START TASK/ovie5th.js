d36e2360b58e4d0886fbee2b5085d57b18261640
Ovie5th <53409530+Ovie5th@users.noreply.github.com>
2020-03-26 23:20:22 +0100

    Add files via upload

---
+++ START TASK/ovie5th.js
@@ -0,0 +1,10 @@
+let my_name = Ovie5th;
+let my_courses = ['html', 'css', 'javascript'];
+console.log(my_name);
+console.log("my courses are " + my_courses);
+
+
+for (let i = 0; i <= 200; i++) {
+
+    if (i % 2 !== 0) console.log(i)
+}
